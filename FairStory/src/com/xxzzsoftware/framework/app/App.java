c959b5947ce7fd6abf21b0a79f2601c6e62bd108
Xu Zhuo <xuzhuo@yhd.com>
2016-04-25 18:11:17 +0800

    init

---
+++ FairStory/src/com/xxzzsoftware/framework/app/App.java
@@ -0,0 +1,12 @@
+package com.xxzzsoftware.framework.app;
+
+import android.app.Application;
+
+public class App extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+}
